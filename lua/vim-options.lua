ba82919fed16e57148d9386e5d3c6da55676aba1
Troy Johnston <troy@cloud-team.com>
2024-05-08 19:39:37 +0000

    Add keymap for close buffer

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,6 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set notermguicolors")
 vim.cmd("set number")
+
+-- Switch to different buffer and close the previous buffer
+vim.keymap.set("n", "<A-n>", ':b#|bd#<CR>', { silent = true })
