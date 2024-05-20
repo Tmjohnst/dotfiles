a27a05e29301d4b87e624ddd790a3e0ce1a51a9a
Troy Johnston <troy@cloud-team.com>
2024-05-20 16:05:25 +0000

    Added some neotree niceties

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,22 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+    local neotree = require("neo-tree")
+    neotree.setup({
+      close_if_last_window = false,
+      filesystem = {
+        follow_current_file = {
+          enabled = true
+        }
+      }
+    })
+    require("neo-tree.sources.manager").focus("filesystem")
+  end,
 }
